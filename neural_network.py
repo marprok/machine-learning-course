6f1563b5589ee4bccd0e60132afb870abaf72714
marprok <mariosprokopakis@gmail.com>
2019-04-06 13:03:14 +0300

    made the whole feed forward work with batches and I think it works(at least it doesn't crash)

--- neural_network.py
+++ neural_network.py
@@ -2,6 +2,7 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 import math
+from random import shuffle
 
 def func1(dot):
     return math.log(1 + math.exp(dot))
@@ -15,11 +16,11 @@ def func2(dot):
 def func2der(dot):
     return 1 - math.pow(func2(dot),2)
 
-def func3(dot):
-    return math.cos(dot)
+def func3(vertex):
+    return np.cos(vertex)
 
-def func3der(dot):
-    return -math.sin(dot)
+def func3der(vertex):
+    return -1*np.sin(vertex)
 
 #use by default ax=1, when the array is 2D
 #use ax=0 when the array is 1D
@@ -44,7 +45,7 @@ def softmax( x, ax=1 ):
 '''
 class NeuNet():
 
-    def __init__(self, hidlen, outlen, func, der):
+    def __init__(self, hidlen, outlen, func, der, batch = 100):
         self.hidlen = hidlen # for the bias term
         self.outlen = outlen
         #self.hidden = np.zeros((1,hidlen))
@@ -56,6 +57,24 @@ class NeuNet():
         self.test = None
         self.inlen = 0
         self.w0 = None
+        self.batch = batch
+
+    def shuffle_data(self):
+        # zip the one hot vector and the training data together
+        temp = list(zip(self.train_data, self.train_1hv))
+        # shuffle them
+        shuffle(temp)
+        # unzip
+        self.train_data, self.train_1hv = zip(*temp)
+    
+    def train_net(self, epochs):
+        for i in range(epochs):
+            print('epoch: ',i)
+            # first we shuffle the training data
+            self.shuffle_data()
+            self.feed_forward()
+            
+        
     def readMnistData(self, path):
         files = [f for f in listdir(path) if isfile(join(path, f))]
         print(files)
@@ -69,32 +88,81 @@ class NeuNet():
         
         for f in self.train[:1]: # read only the first training
             num = int(f.split('train')[1][0])
-            self.train_1hv.append(np.array([1 if i == num else 0 for i in range(10)])) # create the one hot vector
+            
             #print(self.train_1hv)
             #print('the number is ' + str(num))
             with open(join(path,f), 'r') as fl:
                 for line in fl:
                     self.train_data.append(list(map(int, line.split(' '))))
+                    self.train_1hv.append(np.array([1 if i == num else 0 for i in range(10)])) # create the one hot vector
                     #lines.append(line.split(' '))
         #print(lines[3])
         self.train_1hv = np.array([x for x in self.train_1hv])
-        print(self.train_1hv.shape)
-        print(self.train_1hv)
+        #print(self.train_1hv.shape)
+        #print(self.train_1hv)
         
         self.train_data = np.array([np.array(x) for x in self.train_data])
         self.train_data = self.train_data.astype(float) / 255 # normalize the data
         # the last column of the data matrix is the bias column
-        self.train_data = np.hstack((np.ones((self.train_data.shape[0], 1)), self.train_data))  # +1 col at the end of the array for the bias term
+        self.train_data = np.hstack((np.ones((self.train_data.shape[0], 1)), self.train_data))  # +1 col at the start of the array for the bias term
         print(self.train_data.shape)
         # now that we have read the training data, we can initialize the input layer
         self.inlen = len(self.train_data[0])
-        print('inlen: ' + str(self.inlen))
+        #print('inlen: ' + str(self.inlen))
         self.w0 = np.random.rand(self.hidlen,self.inlen) # the 2D array that contains the weights of the first part of the neural network
         self.w1 = np.random.rand(self.outlen,self.hidlen + 1) # the 2D array that contains the weights of the second part of the neural network + 1 for the bias
 
     def feed_forward(self):
-        j = 0
-        for data in self.train_data: # for each training data
+        if self.batch > len(self.train_data):
+            print("ERROR: batch size is greater than the total number of training data")
+            return None
+        
+        begin, end = 0, self.batch
+        while begin < end:
+            print('begin: ' + str(begin) +'\nend: ' + str(end))
+            data_batch = self.train_data[begin:end]
+            data_batch = np.array([x for x in data_batch])
+            temp = data_batch.dot(self.w0.T)
+            hidden_leyer_out = self.activation(temp)
+            hidden_leyer_out = np.hstack((np.ones((hidden_leyer_out.shape[0], 1)), hidden_leyer_out))  # +1 col at the start of the array for the bias term
+            
+            temp = hidden_leyer_out.dot(self.w1.T)
+            final_out = softmax(temp)
+            print('cost: ',self.calculate_cost(final_out, np.array(self.train_1hv[begin:end])))
+            '''for current in range(begin, end):
+                input_vec = np.array(self.train_data[current], ndmin=2)
+                
+                #input_vec = data
+                #input_vec = np.hstack((input_vec, np.array([1], ndmin=2)))# +1 for the bias term
+                #print(input_vec.shape)
+                #print(self.w0.shape)
+                temp = input_vec.dot(self.w0.transpose())
+                #print(temp.shape)
+                #hidden_layer_out = np.array(list(map(self.activation,temp)),ndmin=2)
+                hidden_leyer_out = []
+                #print(temp)
+                #for i in range(temp.shape[1]):
+                #    hidden_leyer_out.append(self.activation(temp[0][i]))
+                hidden_leyer_out = self.activation(temp)
+                hidden_leyer_out = np.array(hidden_leyer_out, ndmin=2)
+                hidden_leyer_out = np.hstack((hidden_leyer_out, np.array([1], ndmin=2)))# +1 for the bias term
+                #print(hidden_leyer_out.shape)
+                temp = hidden_leyer_out.dot(self.w1.transpose())
+                #print(temp)
+                #print(temp.shape)
+                # now temp contains the dot products of the second half
+                # and the only thing that is left is to give it as
+                # input to the softmax function
+                final_out = softmax(temp)
+                #print(final_out)
+                print('cost: ',self.calculate_cost(final_out, self.train_1hv[current]))
+                '''
+            begin = end
+            end += self.batch
+            if end > len(self.train_data):
+                end = len(self.train_data)
+             
+        '''for data in self.train_data: # for each training data
             input_vec = np.array(data, ndmin=2)
             #input_vec = data
             #input_vec = np.hstack((input_vec, np.array([1], ndmin=2)))# +1 for the bias term
@@ -118,10 +186,10 @@ class NeuNet():
             # input to the softmax function
             final_out = softmax(temp)
             print(final_out)
-            print('cost: ',self.calculate_cost(final_out, self.train_1hv[0]))
+            print('cost: ',self.calculate_cost(final_out, self.train_1hv[j]))
             if j == 5:
                 break
-            j += 1
+            j += 1'''
             
     def calculate_cost(self, y, t):
         #print(y)
@@ -144,4 +212,4 @@ if __name__ == '__main__':
     '''
     #print(softmax(np.array( [ [10,20,30,40], [20,50,45,45], [983,39,57,752], [574,575,597,525] ] )))
     net.readMnistData('/home/marios/Downloads/mnistdata')
-    net.feed_forward()
+    net.train_net(2)
