4f7ac6cfd6a9552e2b121009742f03826a0ca269
marprok <mariosprokopakis@gmail.com>
2019-04-18 12:03:58 +0300

    testing branch

--- neural_network.py
+++ neural_network.py
@@ -69,13 +69,35 @@ class NeuNet():
     
     def train_net(self, epochs):
         for i in range(epochs):
-
             # first we shuffle the training data
             self.shuffle_data()
-            cost = self.feed_forward()
+            if self.batch > len(self.train_data):
+                print("ERROR: batch size is greater than the total number of training data")
+                return None
+
+            begin, end = 0, self.batch
+            cost = None
+            while begin < end:
+                data_batch = self.train_data[begin:end]
+                data_batch = np.array([x for x in data_batch])
+                hot_vec = np.array(self.train_1hv[begin:end])
+
+                # print('begin: ' + str(begin) +'\nend: ' + str(end))
+                cost, hidden_out, final_out = self.feed_forward(data_batch, hot_vec)
+                dw0, dw1 = self.compute_gradients_cost(data_batch,hidden_out,final_out,hot_vec, 0.01)
+
+                self.w0 += 0.005*dw0
+                self.w1 += 0.005*dw1
+
+                begin = end
+                end += self.batch
+                if end > len(self.train_data):
+                    end = len(self.train_data)
+
             print('cost: ',cost,' epoch = ', i)
 
-            
+
+
         
     def readMnistData(self, path):
         files = [f for f in listdir(path) if isfile(join(path, f))]
@@ -115,7 +137,7 @@ class NeuNet():
         self.w1 = np.random.rand(self.outlen,self.hidlen + 1) # the 2D array that contains the weights of the second part of the neural network + 1 for the bias
 
 
-    def compute_gradients_cost(self, data_batch, hidden_out, batch_dot_w0, hidden_dot_w1, output, onehotvec,lamda = 0):
+    def compute_gradients_cost(self, data_batch, hidden_out, output, onehotvec,lamda = 0):
         #Z = get_z(X,w1) # hidden_out
 
         # The result of Z*w2
@@ -123,10 +145,10 @@ class NeuNet():
 
         #Y = softmax(z_w2) #output
         # Compute the cost function to check convergence
-        max_error = np.max(hidden_dot_w1, axis=1)
-        Ew = np.sum(onehotvec * hidden_dot_w1) - np.sum(max_error) - \
-             np.sum(np.log(np.sum(np.exp(hidden_dot_w1 - np.array([max_error, ] * hidden_dot_w1.shape[1]).T), 1))) - \
-             (0.5 * lamda) * (np.sum(np.square(self.w0)) + np.sum(np.square(self.w1)))
+        #max_error = np.max(hidden_dot_w1, axis=1)
+        #Ew = np.sum(onehotvec * hidden_dot_w1) - np.sum(max_error) - \
+             #np.sum(np.log(np.sum(np.exp(hidden_dot_w1 - np.array([max_error, ] * hidden_dot_w1.shape[1]).T), 1))) - \
+             #(0.5 * lamda) * (np.sum(np.square(self.w0)) + np.sum(np.square(self.w1)))
         
         # Calculate gradient for w1
         grad_w1 = (onehotvec - output).T.dot(hidden_out) - lamda * self.w1
@@ -143,100 +165,25 @@ class NeuNet():
         grad_w0 = temp.T.dot(data_batch) - lamda*self.w0
         
         
-        return Ew, grad_w0, grad_w1
+        return grad_w0, grad_w1
 
 
-    
-    def feed_forward(self):
+    # should return the cost
+    def feed_forward(self, data_batch, hot_vec):
 
-        if self.batch > len(self.train_data):
-            print("ERROR: batch size is greater than the total number of training data")
-            return None
-        
-        begin, end = 0, self.batch
-        cost = None
-        while begin < end:
-            #print('begin: ' + str(begin) +'\nend: ' + str(end))
-            data_batch = self.train_data[begin:end]
-            data_batch = np.array([x for x in data_batch])
-            temp0 = data_batch.dot(self.w0.T)
-            hidden_leyer_out = self.activation(temp0)
-            hidden_leyer_out = np.hstack((np.ones((hidden_leyer_out.shape[0], 1)), hidden_leyer_out))  # +1 col at the start of the array for the bias term
-            
-            temp1 = hidden_leyer_out.dot(self.w1.T)
-            final_out = softmax(temp1)
-            hot_vec = np.array(self.train_1hv[begin:end])
+        temp0 = data_batch.dot(self.w0.T)
 
-            #print('cost: ',cost)
+        hidden_leyer_out = self.activation(temp0)
+        hidden_leyer_out = np.hstack((np.ones((hidden_leyer_out.shape[0], 1)),
+                                      hidden_leyer_out))  # +1 col at the start of the array for the bias term
 
-            error, dw0, dw1 = self.compute_gradients_cost(data_batch, hidden_leyer_out, temp0, temp1, final_out, hot_vec,lamda = 0.01)
-            self.w0 += 0.005*dw0
-            self.w1 += 0.005*dw1
-            #print('error: ',error)
-            cost = self.calculate_cost(final_out, hot_vec)
-            #print('cost: ', cost)
-            '''for current in range(begin, end):
-                input_vec = np.array(self.train_data[current], ndmin=2)
-                
-                #input_vec = data
-                #input_vec = np.hstack((input_vec, np.array([1], ndmin=2)))# +1 for the bias term
-                #print(input_vec.shape)
-                #print(self.w0.shape)
-                temp = input_vec.dot(self.w0.transpose())
-                #print(temp.shape)
-                #hidden_layer_out = np.array(list(map(self.activation,temp)),ndmin=2)
-                hidden_leyer_out = []
-                #print(temp)
-                #for i in range(temp.shape[1]):
-                #    hidden_leyer_out.append(self.activation(temp[0][i]))
-                hidden_leyer_out = self.activation(temp)
-                hidden_leyer_out = np.array(hidden_leyer_out, ndmin=2)
-                hidden_leyer_out = np.hstack((hidden_leyer_out, np.array([1], ndmin=2)))# +1 for the bias term
-                #print(hidden_leyer_out.shape)
-                temp = hidden_leyer_out.dot(self.w1.transpose())
-                #print(temp)
-                #print(temp.shape)
-                # now temp contains the dot products of the second half
-                # and the only thing that is left is to give it as
-                # input to the softmax function
-                final_out = softmax(temp)
-                #print(final_out)
-                print('cost: ',self.calculate_cost(final_out, self.train_1hv[current]))
-                '''
-            begin = end
-            end += self.batch
-            if end > len(self.train_data):
-                end = len(self.train_data)
-             
-        '''for data in self.train_data: # for each training data
-            input_vec = np.array(data, ndmin=2)
-            #input_vec = data
-            #input_vec = np.hstack((input_vec, np.array([1], ndmin=2)))# +1 for the bias term
-            print(input_vec.shape)
-            print(self.w0.shape)
-            temp = input_vec.dot(self.w0.transpose())
-            print(temp.shape)
-            #hidden_layer_out = np.array(list(map(self.activation,temp)),ndmin=2)
-            hidden_leyer_out = []
-            print(temp)
-            for i in range(temp.shape[1]):
-                hidden_leyer_out.append(self.activation(temp[0][i]))
-            hidden_leyer_out = np.array(hidden_leyer_out, ndmin=2)
-            hidden_leyer_out = np.hstack((hidden_leyer_out, np.array([1], ndmin=2)))# +1 for the bias term
-            print(hidden_leyer_out.shape)
-            temp = hidden_leyer_out.dot(self.w1.transpose())
-            print(temp)
-            print(temp.shape)
-            # now temp contains the dot products of the second half
-            # and the only thing that is left is to give it as
-            # input to the softmax function
-            final_out = softmax(temp)
-            print(final_out)
-            print('cost: ',self.calculate_cost(final_out, self.train_1hv[j]))
-            if j == 5:
-                break
-            j += 1'''
-        return cost
+        temp1 = hidden_leyer_out.dot(self.w1.T)
+
+        final_out = softmax(temp1)
+
+
+        # print('error: ',error)
+        return self.calculate_cost(final_out, hot_vec), hidden_leyer_out, final_out
 
     def calculate_cost(self, y, t):
         #print(y)
@@ -245,7 +192,7 @@ class NeuNet():
         l = 0.01
         return np.sum(y*(t)) - l/2 * (np.linalg.norm(self.w0, 'fro')**2 + np.linalg.norm(self.w1, 'fro')**2)
 
-    def gradcheck_softmax(self, lamda):
+    '''def gradcheck_softmax(self, lamda):
 
 
         epsilon = 1e-6
@@ -298,7 +245,7 @@ class NeuNet():
                 numericalGrad[k, d] = (e_plus - e_minus) / (2 * epsilon)
 
 
-        return (gradEw, numericalGrad)
+        return (gradEw, numericalGrad)'''
 
 
 if __name__ == '__main__':
@@ -316,5 +263,5 @@ if __name__ == '__main__':
     print(func3der(x))
     '''
     #print(softmax(np.array( [ [10,20,30,40], [20,50,45,45], [983,39,57,752], [574,575,597,525] ] )))
-    net.readMnistData('/home/p3150141/Downloads/')
+    net.readMnistData('/home/p3150141/Downloads/mnistdata')
     net.train_net(100)
