2e2efc155ad214cd01298c7eee125fc9c777e41b
marprok <mariosprokopakis@gmail.com>
2019-05-11 17:40:43 +0300

    I have done something and I do not know if it is correct...nice

--- em.py
+++ em.py
@@ -25,36 +25,49 @@ def expectation_maximization(X, K):
     #print(sigma_squared.shape)
     #print(mvals.shape)
     #print(pvals.shape)
-
-    for k in range(K):
-        gvals[:, k] = pvals[k]*np.prod((1/np.sqrt(2*np.pi*sigma_squared[k]))*np.exp(-(1/(2*sigma_squared[k]))*((X - mvals[k])**2)), axis = 1) # numpy allows NxD - 1XD = NxD
-        print('test')
-    gs = np.sum(gvals, axis = 1)
-    for k in range(K):
-        gvals[:, k] = gvals[:, k] / gs
-    
-    print(np.sum(gvals, axis = 1)[:5])
-
-
-
-    
-    '''for n in range(X.shape[0]):
-        temp = []
+    i = 0
+    while i < 10:
+        # expectation stage
         for k in range(K):
-            temp.append(pvals[k]*mix(sigma_squared[k], X[n], mvals[k]).prod(axis=0))
-        temp = np.array(temp)
-        #print(temp.shape)
-        s = np.sum(temp)
-        for k in range(K):
-            gvals[n][k] = temp[k]/s
+            gvals[:, k] = pvals[k]*np.prod((1/np.sqrt(2*np.pi*sigma_squared[k]))*\
+                      np.exp(-(1/(2*sigma_squared[k]))*((X - mvals[k])**2)), axis = 1) # numpy allows NxD - 1XD = NxD
 
-        #print(temp/np.sum(temp))
-        #print(temp.shape)
-        temp.reshape(K)
-        #print(temp.shape)
-        #gvals[n] = temp/np.sum(temp)
-        #print(gvals[n])
-        break'''
+        gs = np.sum(gvals, axis = 1)
+        #print(gs)
+        for k in range(K):
+            gvals[:, k] = gvals[:, k] / gs
+
+        # test the expectation stage
+        #print(np.sum(gvals, axis = 1)[:5])
+        gs = np.sum(gvals,  axis = 0)
+        print('ms shape:', gs.shape)
+        # maximization stage
+        for k in range(K):
+            g = np.array(gvals[:, k], ndmin=2).T
+            t = np.array(X*g, ndmin = 2)
+            #print(t.shape)
+            #print(ms[k].shape)
+            mvals[k] = np.sum(t, axis = 0) / gs[k]
+
+        #print(mvals)
+
+        for k in range(K):
+            temp_dif = X - mvals[k,:]
+            temp_dif = np.power(temp_dif, 2)
+            print('temp_dif', temp_dif.shape)
+            temp = np.array(np.sum(temp_dif, axis = 1), ndmin=2).T
+            print('temp', temp.shape)
+            temp2 = np.array(gvals[:, k], ndmin=2)
+            print('temp2', temp2.shape)
+            sigma_squared[k] = temp2.dot(temp)/(X.shape[1] * gs[k])
+            #temp = temp *
+        #print(sigma_squared)
+
+        for k in range(K):
+            pvals[k] = gs[k]/X.shape[0]
+        
+        i += 1
+
     
 if __name__ == '__main__':
     img = read_image('im.jpg')
